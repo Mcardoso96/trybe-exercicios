33778855e16a7c5f9faa236298de7ba2a98c94b8
Marcos_Cardoso <marcoscardoso825@gmail.com>
2022-05-20 18:47:55 -0300

    dia 5.1 todo o conteúdo do course

---
+++ Fundamentos/bloco-05-Js-DOM-Eventos-Web-storage/dia-01-O DOM-Ou-como-o-HTML-e-o-JavaScript-se-comunicam/Get-Element-By-Id/script.js
@@ -0,0 +1,8 @@
+console.log(document.getElementById('page-title'))
+/*console.log(document.getElementById('second-paragraph').innerText/innerHTML);*/ //testando o .innerText e o .innerHTML.
+
+document.getElementById('page-title').innerText = "Toy Story";
+
+document.getElementById('second-paragraph').innerText = 'O exercício me pediu para fazer isso, só estou obedecendo!'
+
+document.getElementById('subtitle').innerText = 'Modificando essa bagaça!'
