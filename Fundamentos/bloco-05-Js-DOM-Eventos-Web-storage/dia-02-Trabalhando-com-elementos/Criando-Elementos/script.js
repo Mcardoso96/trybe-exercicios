d4d3028c6714a35ffc9ef8457e6f7dac3a368112
Marcos_Cardoso <marcoscardoso825@gmail.com>
2022-05-22 10:23:47 -0300

    Parte2- crindo elementos

---
+++ Fundamentos/bloco-05-Js-DOM-Eventos-Web-storage/dia-02-Trabalhando-com-elementos/Criando-Elementos/script.js
@@ -0,0 +1,24 @@
+//Questão 1
+let pai = document.querySelector('#pai');
+let novoFilho = document.createElement('section');
+console.log(novoFilho);
+pai.appendChild(novoFilho);
+document.querySelector('#pai').children
+
+//Questão 2
+let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+let filhoOndeVcEsta = document.createElement('section');
+console.log(filhoOndeVcEsta);
+elementoOndeVoceEsta.appendChild(filhoOndeVcEsta);
+document.querySelector('#elementoOndeVoceEsta').children
+
+//Questão 3
+let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+let netoOndeVcEsta = document.createElement('section');
+console.log(netoOndeVcEsta);
+primeiroFilhoDoFilho.appendChild(netoOndeVcEsta);
+console.log(document.querySelector('#primeiroFilhoDoFilho').children)
+
+//Questão 4
+netoOndeVcEsta.id = 'netoOndeVcEsta'
+console.log(document.querySelector('#netoOndeVcEsta').parentElement.parentElement.nextElementSibling)
