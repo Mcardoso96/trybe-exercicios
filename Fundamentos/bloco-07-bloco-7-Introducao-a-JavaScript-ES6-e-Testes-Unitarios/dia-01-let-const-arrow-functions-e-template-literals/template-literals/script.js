4481e965599c624f4bc45d730e9c64cebfec5695
Marcos_Cardoso <marcoscardoso825@gmail.com>
2022-06-09 11:25:35 -0300

    template literals

---
+++ Fundamentos/bloco-07-bloco-7-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-01-let-const-arrow-functions-e-template-literals/template-literals/script.js
@@ -0,0 +1,18 @@
+// Template literals
+
+// concatenação de strings do jeito antigo
+/* const myName = 'Isabella';
+console.log('Hello' + ' ' + myName + '!'); */
+
+// concatenação de strins com template literals
+/* const myName = 'Isabella';
+console.log(`Welcome ${myName}!`); */
+
+// Com o template literals
+/* console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n'); */
